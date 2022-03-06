415cca308950c36482f91e30bdbb6fdf8f26ea4a
Abraham Medina Carrillo <abraham.medina.carrillo@uabc.edu.mx>
2022-03-06 14:59:05 -0800

    default lang html, test http post to server php

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
 # feyri-labs
 
-## Project setup
+## Run server php
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
+  php -S localhost:8000 -t .\server\
 ```
 
 ### Compiles and minifies for production
 ```
 npm run build
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="printData">PrintData</button>
   </div>
 </template>
 
@@ -9,10 +10,15 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  printData() {
+    fetch("/labs", { method: "post" }).then(data => data.json()).then(result => console.log(result))
+  }
+}
 </script>
